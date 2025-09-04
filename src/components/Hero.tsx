6a607ed35fb0c7fc861c9c20acefb62d4686e150
LittleKenzy <keygaming589@gmail.com>
2025-09-04 16:47:44 +0700

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { cn } from '@/lib/utils/cn'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
+    <div className='pb-20 pt-20'>
       <div>
         <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
         <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
@@ -27,9 +29,15 @@ const Hero = () => {
 
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-          <h1 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 '>Dynamic web magic with next. js</h1>
+          <h1 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic web magic with next. js</h1>
 
-          <TextGenerateEffect words='Transforming your ideas into captivating web experiences' className=''/>
+          <TextGenerateEffect words='Transforming your ideas into captivating web experiences' className='text-center text-[40px] md:text-5xl lg:text-6xl'/>
+
+          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>Hi, I&apos;m <span className='text-blue-200'>Little Kenzy</span> a web developer from <span className='text-blue-200'>Indonesian</span>.</p>
+
+          <a href="#about">
+            <MagicButton title='Show my work' icon={<FaLocationArrow />} position='right'/>
+          </a>
         </div>
       </div>
     </div>
