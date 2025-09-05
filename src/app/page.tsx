944962533341b2dac3ce94c319d302733ed247d3
LittleKenzy <keygaming589@gmail.com>
2025-09-05 21:45:01 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/Grid";
 import { FaHome } from "react-icons/fa";
+import RecentProjcects from "@/components/RecentProjcects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          { name: 'Home', link: '/', icon: <FaHome /> }
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid /> 
+        <Grid />
+        <RecentProjcects />
       </div>
     </main>
   );
